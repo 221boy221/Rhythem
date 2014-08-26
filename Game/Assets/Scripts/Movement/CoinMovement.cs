2649ef035c40403cceb5c87a5ede5f7224d845ba
AlexAntonides <AlexAntonides@github.com>
2014-08-26 16:06:05 +0200

    Coins & Arch Generator

---
+++ Game/Assets/Scripts/Movement/CoinMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinMovement : MonoBehaviour {
+    /* 
+     * Dit script is gemaakt door Alex Antonides © - 2014.
+     */
+    private float movementSpeed = 0.1f;
+    void Update() {
+        transform.Translate(Vector3.left * movementSpeed);
+	}
+
+    void OnCollisionEnter2D(Collision2D player) {
+        if (player.gameObject.tag == "Player") {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D wall) {
+        if (wall.gameObject.tag == "deathWall") {
+            Destroy(gameObject);
+        }
+    }
+}
