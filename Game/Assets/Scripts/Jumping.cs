05244a560af18be6e335168a5f9350f4259f3470
Lerondo <alessandrojbruin@hotmail.com>
2014-08-26 16:21:25 +0200

    Commit 26-8-2014 AB

---
+++ Game/Assets/Scripts/Jumping.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jumping : MonoBehaviour {
+
+	public int jumpForce = 250;
+	public float jumpSpeed = 3f;
+	private bool onGround = true;
+	
+	void Update()
+	{			
+		//If you're on the ground you will be able to jump
+		if(onGround == true)
+		{
+			//If W is pressed you will Jump
+			if(Input.GetKeyDown(KeyCode.W))
+			{
+				Jump();
+			}
+		}
+	}
+	
+	//Jump Physics
+	void Jump()
+	{
+		this.transform.rigidbody2D.AddForce(Vector3.up * jumpSpeed * jumpForce);
+		onGround = false;
+	}
+	
+	//Checks if you're on the ground
+	void OnCollisionEnter2D (Collision2D hit)
+	{
+		if(hit.transform.tag == "ground")
+		{
+			onGround = true;
+		}
+	}
+}
---
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed = 0.1f;
+	private float xMin = -8;
+	private float xMax = 8;
+	
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.A) && transform.position.x > xMin) 
+		{
+			transform.Translate(Vector3.left * speed);
+		}
+		if (Input.GetKey (KeyCode.D)  && transform.position.x < xMax) 
+		{
+			transform.Translate(Vector3.right * speed);
+		}
+	}
+}
