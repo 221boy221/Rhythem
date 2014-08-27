2754c43ef4a4d609807f610bcd23dcbda690602d
Boy Voesten <warrock.boy@hotmail.com>
2014-08-27 09:51:14 +0200

    Merged certain scrips for the player together in one, PlayerController, created player prefab, put all the created stuff together into 1 scene, prototype01. Fixed errors.

---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //Movement
+	public float speed = 0.1f;
+	private float xMin = -8f;
+	private float xMax = 8f;
+    //Jumping
+    public int jumpForce = 250;
+    public float jumpSpeed = 3f;
+    private bool onGround = true;
+	
+	void Update () {
+        Movement();
+        Jumping();
+	}
+
+    private void Movement() {
+        if (Input.GetKey(KeyCode.A) && transform.position.x > xMin) {
+            transform.Translate(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D) && transform.position.x < xMax) {
+            transform.Translate(Vector3.right * speed);
+        }
+    }
+
+    private void Jumping() {
+        // If W is pressed and player is onGround
+        if (Input.GetKeyDown(KeyCode.W) && onGround == true) {
+            //Jump Physics
+            this.transform.rigidbody2D.AddForce(Vector3.up * jumpSpeed * jumpForce);
+            onGround = false;
+        }
+    }
+
+    //Checks if you're on the ground
+    private void OnCollisionEnter2D(Collision2D hit) {
+        if (hit.transform.tag == "ground") {
+            onGround = true;
+        }
+    }
+
+}
