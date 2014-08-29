e9cd52f64059eeb2abc3ea7e6f810e27776146ba
Boy Voesten <warrock.boy@hotmail.com>
2014-08-29 15:27:30 +0200

    final game?

---
+++ Game/Assets/Scripts/WinChecker.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinChecker : MonoBehaviour {
+    public float timeOfSong;
+    public Transform donut;
+    private float timer;
+    private bool isFinished = false;
+	void Update () {
+        timer += Time.deltaTime;
+        checkIfSongEnded();
+	}
+
+    void checkIfSongEnded() {
+        if (timer >= timeOfSong && isFinished == false){
+            Instantiate(donut, transform.position, transform.rotation);
+            isFinished = true;
+        }
+    }
+}
---
+++ Game/Assets/Scripts/WinLine.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinLine : MonoBehaviour {
+    void OnTriggerEnter2D(Collider2D wall) {
+        if (wall.gameObject.tag == "deathWall") {
+            Destroy(gameObject);
+        }
+
+        if (wall.gameObject.tag == "Player") {
+            Destroy(wall.gameObject);
+            DataClass.scoreInt = 0;
+            Application.LoadLevel("MainMenu");
+        }
+    }
+}
